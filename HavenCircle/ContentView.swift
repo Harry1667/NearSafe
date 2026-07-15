5b19353b14b42d611eb9c4caed20572089eae56b
Harry <amacooky.com@gmail.com>
2026-07-15 11:22:57 +0800

    Initial Commit

---
+++ HavenCircle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HavenCircle
+//
+//  Created by Harry Hwa on 2026/7/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HavenCircle/HavenCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HavenCircleApp.swift
+//  HavenCircle
+//
+//  Created by Harry Hwa on 2026/7/15.
+//
+
+import SwiftUI
+
+@main
+struct HavenCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
